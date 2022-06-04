bb4d72ce4343697b6586ea2e2e29877673ad8143
caiohscruz <caiohscruz@hotmail.com>
2022-06-04 17:18:13 -0300

    Cria interfaces para padronizar DAOs inspirado no padrão CQRS

---
+++ src/Alura.LeilaoOnline.WebApp/Dados/ICommand.cs
@@ -0,0 +1,12 @@
+﻿namespace Alura.LeilaoOnline.WebApp.Dados
+{
+    public interface ICommand<T>
+    {
+        void Incluir(T obj);
+        void Alterar(T obj);
+        void Excluir(T obj);
+
+
+
+    }
+}
---
+++ src/Alura.LeilaoOnline.WebApp/Dados/IQuery.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace Alura.LeilaoOnline.WebApp.Dados
+{
+    public interface IQuery<T>
+    {
+        IEnumerable<T> BuscarTodos();
+        T BuscarPorId(int id);
+
+    }
+}
