d16a8da0e56f67396a36246be86d58517df31ffd
caiohscruz <caiohscruz@hotmail.com>
2022-06-04 14:49:47 -0300

    Retira da HomeController a responsabilidade de manipular os dados e injeta as DAOs

--- src/Alura.LeilaoOnline.WebApp/Controllers/HomeController.cs
+++ src/Alura.LeilaoOnline.WebApp/Controllers/HomeController.cs
@@ -9,26 +9,18 @@ namespace Alura.LeilaoOnline.WebApp.Controllers
 {
     public class HomeController : Controller
     {
-        AppDbContext _context;
+        ILeilaoDao _leilaoDao;
+        ICategoriaDao _categoriaDao;
 
-        public HomeController()
+        public HomeController(ILeilaoDao leilaoDao, ICategoriaDao categoriaDao)
         {
-            _context = new AppDbContext();
+            _leilaoDao = leilaoDao;
+            _categoriaDao = categoriaDao;
         }
 
         public IActionResult Index()
         {
-            var categorias = _context.Categorias
-                .Include(c => c.Leiloes)
-                .Select(c => new CategoriaComInfoLeilao
-                {
-                    Id = c.Id,
-                    Descricao = c.Descricao,
-                    Imagem = c.Imagem,
-                    EmRascunho = c.Leiloes.Where(l => l.Situacao == SituacaoLeilao.Rascunho).Count(),
-                    EmPregao = c.Leiloes.Where(l => l.Situacao == SituacaoLeilao.Pregao).Count(),
-                    Finalizados = c.Leiloes.Where(l => l.Situacao == SituacaoLeilao.Finalizado).Count(),
-                });
+            var categorias = _categoriaDao.BuscarCategoriasPorInfoLeilao();
             return View(categorias);
         }
 
@@ -42,9 +34,7 @@ namespace Alura.LeilaoOnline.WebApp.Controllers
         [Route("[controller]/Categoria/{categoria}")]
         public IActionResult Categoria(int categoria)
         {
-            var categ = _context.Categorias
-                .Include(c => c.Leiloes)
-                .First(c => c.Id == categoria);
+            var categ = _categoriaDao.BuscarCategoriaPorId(categoria);
             return View(categ);
         }
 
@@ -53,12 +43,7 @@ namespace Alura.LeilaoOnline.WebApp.Controllers
         public IActionResult Busca(string termo)
         {
             ViewData["termo"] = termo;
-            var termoNormalized = termo.ToUpper();
-            var leiloes = _context.Leiloes
-                .Where(c =>
-                    c.Titulo.ToUpper().Contains(termoNormalized) ||
-                    c.Descricao.ToUpper().Contains(termoNormalized) ||
-                    c.Categoria.Descricao.ToUpper().Contains(termoNormalized));
+            var leiloes = _leilaoDao.BuscarLeiloesPorTermo(termo);
             return View(leiloes);
         }
     }
