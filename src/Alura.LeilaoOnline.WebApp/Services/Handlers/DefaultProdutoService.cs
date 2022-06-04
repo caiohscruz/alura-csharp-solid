dd48ba4e4c52a1fa1cf9c524734fd4eb3888ed65
caiohscruz <caiohscruz@hotmail.com>
2022-06-04 15:27:57 -0300

    Cria camada de serviços para tratar dos casos de uso

---
+++ src/Alura.LeilaoOnline.WebApp/Services/Handlers/DefaultProdutoService.cs
@@ -0,0 +1,34 @@
+﻿using Alura.LeilaoOnline.WebApp.Dados;
+using Alura.LeilaoOnline.WebApp.Models;
+using System.Collections.Generic;
+
+namespace Alura.LeilaoOnline.WebApp.Services.Handlers
+{
+    public class DefaultProdutoService : IProdutoService
+    {
+        ILeilaoDao _leilaoDao;
+        ICategoriaDao _categoriaDao;
+
+        public DefaultProdutoService(ILeilaoDao leilaoDao, ICategoriaDao categoriaDao)
+        {
+            _leilaoDao = leilaoDao;
+            _categoriaDao = categoriaDao;
+        }
+
+        public IEnumerable<Categoria> BuscarCategoriasComInfoLeilao()
+        {
+            return _categoriaDao.BuscarCategoriasComInfoLeilao();
+        }
+
+        public Categoria BuscarCategoriaPorId(int id)
+        {
+            return _categoriaDao.BuscarCategoriaPorId(id);
+        }
+
+        public IEnumerable<Leilao> BuscarLeiloesPorTermo(string termo)
+        {
+            return _leilaoDao.BuscarLeiloesPorTermo(termo);
+        }
+        
+    }
+}
---
+++ src/Alura.LeilaoOnline.WebApp/Services/IProdutoService.cs
@@ -0,0 +1,12 @@
+﻿using Alura.LeilaoOnline.WebApp.Models;
+using System.Collections.Generic;
+
+namespace Alura.LeilaoOnline.WebApp.Services
+{
+    public interface IProdutoService
+    {
+        IEnumerable<Categoria> BuscarCategoriasComInfoLeilao();
+        Categoria BuscarCategoriaPorId(int id);
+        IEnumerable<Leilao> BuscarLeiloesPorTermo(string termo);
+    }
+}
